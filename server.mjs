48e6194d02dbf6e399547fb10eb60788714997c2
Jonathan Cortoppassi <106403752+cortoppassi@users.noreply.github.com>
2023-03-08 08:50:20 -0300

    Update server.mjs
    A biblioteca Puppeteer é uma biblioteca node.Js que fornece uma API de alto nível para controlar o chromium sobre o protocolo devtools.

--- server.mjs
+++ server.mjs
@@ -1,32 +1,15 @@
 import puppeteer from 'puppeteer';
-import express from 'express';
-// const puppeteer = require('puppeteer');
-const app = express();
-const port = 3000;
 
-app.get('/', async(req, res) =>{
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto('https://www.sofista.com.br/');
-//   // await page.screenshot({path: 'example.png'});
-
-// const conteudo = await page.evaluate(() =>{
-//   return{
-//     h1: document.querySelector('bo.fw-bolder.f-size-custom-resp'),innerHtml
-//   };
-// })
+(async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
   
-// console.log(conteudo)
-// await browser.close();
-
-  res.send({
-    
-
-  });
+  await page.goto('https://aluno.unifacs.br/SOL/aluno/index.php/index/seguranca/dev/instituicao/127')
+  await page.type("input[name='matricula']", '*********');
+  await page.type("input[name='senha']", '*********');
+  await page.click("input[name=logar]");
+  
+   
 });
-
-app.listen(port, () =>{
-  console.log(`Servidor rodando na porta http://localhost:${port}`)
-})
-
-//================================================================
+  // https://pptr.dev/
+  // https://www.youtube.com/watch?v=xianIw6PleE
