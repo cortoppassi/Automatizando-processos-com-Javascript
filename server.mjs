4fab74e9095318910e6400f777fae368ce30c5c6
Jonathan Cortoppassi <106403752+cortoppassi@users.noreply.github.com>
2023-03-09 01:29:15 -0300

    Update index.mjs
    Automatizando processos com uma API de alto nível para controlar o chromium sobre protocolo Devtools.

--- server.mjs
+++ server.mjs
@@ -3,13 +3,21 @@ import puppeteer from 'puppeteer';
 (async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  
-  await page.goto('https://aluno.unifacs.br/SOL/aluno/index.php/index/seguranca/dev/instituicao/127')
-  await page.type("input[name='matricula']", '*********');
-  await page.type("input[name='senha']", '*********');
-  await page.click("input[name=logar]");
-  
-   
+  try {
+    await page.goto('https://aluno.unifacs.br/SOL/aluno/index.php')
+    await page.type("input[name='matricula']", '**********');
+    await page.type("input[name='senha']", '**********');
+    await page.click("input[name=logar]");
+    await page.waitForNavigation();
+    await page.goto('https://aluno.unifacs.br/SOL/aluno/index.php/ead/ilang');
+    await page.click("input[name=btnSalaAulaVirtual]");
+    await page.waitForNavigation();
+    await page.goto('https://student.ulife.com.br/StudentHome#pageIndex=1');
+    await page.waitForNavigation();
+    await page.click(".ng-scope");
+
+  } catch (err) {
+    console.log(err.msg)
+  }
+  //  await page.close();
 })();
-  // https://pptr.dev/
-  // https://www.youtube.com/watch?v=xianIw6PleE
